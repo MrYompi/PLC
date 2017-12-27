a4e345d825e468fdc881189b2b60aa6eafe17694
MrYompi <Ojedamorala@hotmail.com>
2017-12-27 19:43:00 +0100

    Add files via upload

--- PLC.java
+++ PLC.java
@@ -3,9 +3,14 @@ import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class PLC {
-	public static int tag=0;	   
+	public static int tag=0;
+	public static int tmp=0;
+	public static int as=0;
+	public static HashMap<String, String> table = new HashMap<String, String>();
 	public static PrintStream out;
 	public static void main(String argv[]) {
     try {
@@ -23,4 +28,16 @@ public class PLC {
       e.printStackTrace();
     }
   }
+	public static String get(String s){
+		String s1="";
+		if(table.containsKey(s)){
+			s1=table.get(s);
+		}
+		return s1;
+	}
+	public static String head(String s){
+		Scanner sc =new Scanner(s);
+		sc.useDelimiter("=");
+		return sc.next();
+	}
 }
